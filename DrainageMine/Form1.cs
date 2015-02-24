6aca7124ad0e2d9abfdd9f973f1d1d0b7f301ef3
jcaballo <julien.caballol@hotmail.fr>
2015-02-24 11:52:21 +0100

    Dataset

--- DrainageMine/Form1.cs
+++ DrainageMine/Form1.cs
@@ -19,8 +19,26 @@ namespace DrainageMine
             //Initialisation des valeurs
             coTextBox.Text = "0";
             chTextBox.Text = "0";
-            waterLevelTextBox.Text = "200";
+            waterLevelTextBox.Text = "test";
+            
+            
 
         }
+
+        private void espaceTupleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.espaceTupleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.minageDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: cette ligne de code charge les données dans la table 'minageDataSet.EspaceTuple'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
+            this.espaceTupleTableAdapter.Fill(this.minageDataSet.EspaceTuple);
+
+        }
+
     }
 }
